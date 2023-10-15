bba2e89a869a23c7320157ddfef266bff5911f5a
Anton Bachurin <ant.bachurin@gmail.com>
2023-10-15 21:13:27 +0400

    Исправил снова второе задание. Теперь работает

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,12 @@
 let start = document.getElementById("timer");
 
-let timerId = setInterval(() => start.textContent--, 100);
+let timerId = setInterval(() => {
+  start.textContent--;
+  if (start.textContent === "0") {
+    clearInterval(timerId); 
+    alert("Вы победили в конкурсе!");
+  }
+},
+100);
 
-if (start.textContent === "0") {
-  clearInterval(timerId); 
-  alert("Вы победили в конкурсе!");
-}
 
