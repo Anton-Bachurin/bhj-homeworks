1521e01a89b54abc0e21800bff57257c89d05643
Anton Bachurin <ant.bachurin@gmail.com>
2023-12-13 13:57:20 +0400

    Исправил, но пока не работает правильно

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,23 @@
 const rotator = [...document.querySelectorAll('.rotator__case')];
+let index = 0;
 
-for (let i = 0; i < rotator.length; i++) {
-  rotator[i].classList.remove('rotator__case_active');
-  setInterval(() => {
-    let index = 0;
-    rotator[index].classList.add('rotator__case_active');
-    index++;
-  }, 1000);
-}
+
+
+const intervalId = setInterval(() => {
+  if (index < rotator.length - 1) {
+    rotator[index].classList.remove('rotator__case_active');
+    rotator[index + 1].classList.add('rotator__case_active');  
+  } else if (index === rotator.length - 1) {
+    rotator[index + 1].classList.remove('rotator__case_active');
+    index = 0;
+    rotator[index].classList.add('rotator__case_active');  
+  }
+  
+  console.log(index);
+  index++;
+}, 1000);
+
+
+
 
 
--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,16 +1,23 @@
 const parent = [...document.querySelectorAll('.font-size')];
+const bookContent = document.querySelector('.book__content');
+const fontSizes = ["book_fs-small", "", "book_fs-big"];
 
-for(let i = 0; i < parent.length; i++) {
-  let link = parent[i];
+document.addEventListener('click', elem => {
+  elem.preventDefault();
+  const letter = elem.target.closest('.font-size');
+  
+  if (!letter) return;
 
-  link.onclick = function() {
-  	this.setAttribute('class', 'font-size font-size_active');
-    this.previousElementSibling.setAttribute('class', 'font-size');
-    this.nextElementSibling.setAttribute('class', 'font-size');
-    return false;
-  };
-};
-
+  for(let i = 0; i < parent.length; i++) {
+    let link = parent[i];
+    link.classList.remove('font-size_active');
+    letter.classList.add('font-size_active');
+    
+    if (link.classList.contains('font-size_active')) {
+      bookContent.classList.add(fontSizes[i]);
+    }
+  } 
+});
 
 
 
